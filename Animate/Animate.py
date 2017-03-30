9aa6c425701a7056cedc4b3914a5a3a2bdf8fd9a
Mohar, Boaz <moharb@janelia.hhmi.org>
2017-03-29 23:02:54 -0400

    finished time label helper function, the rest is WIP

--- Animate/Animate.py
+++ Animate/Animate.py
@@ -7,14 +7,14 @@ import matplotlib as mpl
 
 class Animation:
     """ Class to make animation of movies with traces
-    Default behavior is to add all image animation to the top row of subplots 
-    and add all the trace animation plots to the second row
+        Adds all image animation to a top row of subplots 
+        and adds all the trace animation plots to rows 2, 3, ...
 
     """
 
-    def __init__(self, base_path, name, style=None, dt=1.0 / 14):
+    def __init__(self, base_path, name, style, dt=1.0 / 14):
         """
-        
+
         :param base_path: location to save animation 
         :param name: nae of animation
         :param style: same as matplotlib.style.set mainly a dict with rcparams key-value pairs.
@@ -30,22 +30,22 @@ class Animation:
         self.n_trace_plots = 0
         if style is not None:
             plt.style.use(style)
-        self.styles = copy.deepcopy(plt.style.library)
+        self.styles = copy.deepcopy(plt.style.library)  # type: dict
         self._add_styles()
 
     def _add_styles(self):
-        """ Add 4 new styles to the original matplotlib library: dark and light version for images and traces
-        
+        """ Add 4 new styles to the original matplotlib library: dark and light versions for images and traces
+
         """
         styles = dict()
-        default = mpl.rcParamsDefault
+        default = mpl.rcParamsDefault  # for light backgrounds
         dark_background = plt.style.library['dark_background']
 
         dark_img = copy.deepcopy(dark_background)
-        dark_img.update({u'axes.spines.top': False, 'axes.spines.right': False,
-                         u'axes.spines.bottom': False, 'axes.spines.left': False,
-                         u'axes.facecolor': (1, 1, 1, 0), 'axes.edgecolor': (1, 1, 1, 0),
-                         u'xtick.color': (1, 1, 1, 0), 'ytick.color': (1, 1, 1, 0), 'grid.alpha': 0,
+        dark_img.update({u'axes.spines.top': False, u'axes.spines.right': False,
+                         u'axes.spines.bottom': False, u'axes.spines.left': False,
+                         u'axes.facecolor': (1, 1, 1, 0), u'axes.edgecolor': (1, 1, 1, 0),
+                         u'xtick.color': (1, 1, 1, 0), u'ytick.color': (1, 1, 1, 0), u'grid.alpha': 0,
                          u'image.interpolation': 'None'})
         styles[u'dark_img'] = dark_img
 
@@ -75,15 +75,34 @@ class Animation:
 
         self.styles.update(styles)
 
-    def add_label(self, axis=0, location=(0.01, 0.9), s_format='%', size=14):
-        self.label_list.append({'axis': axis, 'location': location, 'format': s_format, 'size': size})
+    def add_label(self, values, axis=0, location=(0.01, 0.9), s_format='%', size=14, **kwargs):
+        """
+        
+        :param values: list of values
+        :param axis: axis number to add the label to
+        :param location: where to add the label in axis coordinates
+        :param s_format: string format to use on the values
+        :param size: font size
+        :param kwargs: to be sent to the plt.text function
+        :return: 
+        """
+        self.label_list.append({'values': values, 'axis': axis, 'location': location, 'format': s_format, 'size': size})
 
-    def add_time_label(self, axis=0, location=(0.01, 0.08), s_format='%.2fs', size=14):
-        self.add_label(axis, location, s_format, size)
+    def add_time_label(self, values=None, axis='img_0', location=(0.01, 0.08), s_format='%.2fs', size=14):
+        if values is None:
+            if len(self.img_list) == 0:
+                if len(self.trace_list) == 0:
+                    raise RuntimeError('Can not add time labels when no values are given and no data was added')
+                else:
+                    values = np.arange(self.trace_list[0]['data'].shape[0]) * self.dt
+            else:
+                values = np.arange(self.img_list[0]['data'].shape[0]) * self.dt
+        self.add_label(values, axis, location, s_format, size)
 
     def add_image_plot(self, data, style='dark_img', c_title=None, ylim_type='p_top', ylim_value=0.1):
         """
         
+        :type ylim_value: Union(float, tuple)
         :param data: 3d array (n, x, y)
         :param c_title: title to put on the color bar
         :param ylim_type: how to set the y limits. 'p_top' will clip the top ylim_value values in %. \
@@ -111,14 +130,16 @@ class Animation:
             img['ymin'] = self.img_list[ylim_value]['ymin']
             img['ymax'] = self.img_list[ylim_value]['ymax']
         elif ylim_type == 'p_top':
+            # noinspection PyTypeChecker
             img['ymax'] = np.nanpercentile(data, 100.0 - ylim_value)
             img['ymin'] = np.nanmin(data)
         elif ylim_type == 'p_bottom':
             img['ymax'] = np.nanmax(data)
             img['ymin'] = np.nanpercentile(data, ylim_value)
         elif ylim_type == 'p_both':
+            # noinspection PyTypeChecker
             img['ymax'] = np.nanpercentile(data, 100.0 - ylim_value)
             img['ymin'] = np.nanpercentile(data, ylim_value)
         else:
-            raise RuntimeError("Expected 'p_top', 'p_top', 'p_top', 'set' or 'same' got: %s" % ylim_type)
+            raise RuntimeError("Expected 'p_top', 'p_bottom', 'p_both', 'set' or 'same' got: %s" % ylim_type)
         self.img_list.append(img)
