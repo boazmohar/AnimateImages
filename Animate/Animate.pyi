a1135b301400cdcfc44388a99bd50a5c085147a6
Mohar, Boaz <moharb@janelia.hhmi.org>
2017-03-29 23:01:51 -0400

    Better type hinting with added typing module to requirements

--- Animate/Animate.pyi
+++ Animate/Animate.pyi
@@ -1,13 +1,22 @@
 from numpy import ndarray
+from typing import Union
 
 class Animation:
-    def __init__(self, base_path: str, name: str, style: tuple, dt: float):
-        pass
-    def add_label(self, axis: int, location: tuple, s_format: str, size: int):
+    def __init__(self, base_path: str, name: str, style: Union(tuple, str), dt: float):
+        self.dt = 0
+        self.trace_list = []
+        self.img_list = []
+        self.label_list = []
+        self.styles = []
+
+    def add_label(self, values: Union(ndarray, list), axis: int, location: tuple, s_format: str, size: int):
         pass
 
-    def add_time_label(self, axis: int, location: tuple, s_format: str, size: int):
+    def add_time_label(self, values: Union(ndarray, list), axis: int, location: tuple, s_format: str, size: int):
         pass
 
-    def add_image_plot(self, data: ndarray, ylim_type: str):
+    def add_image_plot(self, data: ndarray, ylim_type: str, ylim_value: Union(float, tuple, list)):
+        pass
+
+    def _add_styles(self):
         pass
