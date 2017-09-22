415aafdbbc83a34e5a16c1827b66b7c0f5bf6338
Boaz Mohar <boazmohar@gmail.com>
2017-09-22 17:12:02 -0400

    added test for failed saving and fix bug for skipif

--- test/test_save.py
+++ test/test_save.py
@@ -5,7 +5,7 @@ from Animate.Movie import Movie
 from matplotlib.animation import writers
 
 
-@pytest.mark.skipif(len(writers.avail) == 0, 'No writers to save with')
+@pytest.mark.skipif(len(writers.avail) == 0, reason='No writers to save with')
 def test_save(tmpdir):
     path = tmpdir.join('test').relto('')
     m = Movie(dt=1.0/14, height_ratio=2)
@@ -13,3 +13,14 @@ def test_save(tmpdir):
     m.add_image(img, style='dark_img')
     m.save(path)
     assert os.path.isfile(path + '.mp4') or os.path.isfile(path + '.gif')
+
+
+def test_save_fail(tmpdir):
+    path = tmpdir.join('test').relto('')
+    writers.avail = []
+    m = Movie(dt=1.0 / 14, height_ratio=2)
+    img = np.arange(100).reshape(4, 5, 5)
+    m.add_image(img, style='dark_img')
+    with pytest.raises(ValueError) as ex:
+        m.save(path)
+    assert 'Could not find "ffmpeg" and "imagemagick"' in str(ex.value)
